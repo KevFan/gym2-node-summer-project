4ad2428bd77cf7f420eb106a958e5aa213445cbb
KevFan <kevin_fan@hotmail.co.uk>
2017-07-13 13:21:39 +0100

    Update views, server.js & package.json with repo/project info

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ const routes = require('./routes');
 app.use('/', routes);
 
 const listener = app.listen(process.env.PORT || 4000, function () {
-  logger.info(`glitch-template-2 started on port ${listener.address().port}`);
+  logger.info(`Node Gym started on port ${listener.address().port}`);
 });
