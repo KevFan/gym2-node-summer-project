128f0ef536e821104d8b0ff37d2f3e84ecdebf47
KevFan <kevin_fan@hotmail.co.uk>
2017-08-06 13:03:25 +0100

    Removed unused handlebar helpers + seperate session options, update forms to own partials

--- controllers/handlebarHelpers.js
+++ controllers/handlebarHelpers.js
@@ -2,8 +2,6 @@
 
 const goalStore = require('../models/goal-store');
 const classStore = require('../models/class-store');
-const members = require('../models/member-store');
-const analytics = require('../utils/analytics');
 
 const Handlebars = require('handlebars');
 Handlebars.registerHelper('checkForOpenGoals', function (userId) {
@@ -20,27 +18,6 @@ Handlebars.registerHelper('checkForOpenGoals', function (userId) {
   }
 
   return result;
-});
-
-Handlebars.registerHelper('getMemberStats', function (userId) {
-  let stats = analytics.generateMemberStats(members.getMemberById(userId));
-  return new Handlebars.SafeString(
-    '<div class="meta">' + 'BMI: ' + stats.bmi + '</div>' +
-    '<div class="meta">' + 'IsIdealWeight: ' + stats.isIdealBodyweight + '</div>' +
-    '<div class="meta">' + 'BMI Category: ' + stats.bmiCategory + '</div>');
-  //   '<section class="ui two column grid">' +
-  //     '<section class="meta column">\n' +
-  //       '<div class="row">BMI:</div>\n' +
-  //       '<div class="row">IsIdealWeight:</div>\n' +
-  //       '<div class="row">BMI Category: </div>\n' +
-  //     '</section>' +
-  //     '<section class="meta column">\n' +
-  //     '<div class="row">' + stats.bmi + '</div>\n' +
-  //     '<div class="row">' + stats.isIdealBodyweight + '</div>\n' +
-  //     '<div class="row">' + stats.bmiCategory + '</div>\n' +
-  //     '</section>' +
-  //   '</section>'
-  // );
 });
 
 Handlebars.registerHelper('checkEnrolled', function (classId, sessionId, userId) {
